7f02f76d4458f070584f70a86bd8189d1269daa9
Drew J. Sonne <drew.sonne@nordcloud.com>
2016-05-10 09:37:50 +0100

    Initial python commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aws-rds-manager',
+    version='v.1.0.0',
+    packages=['awsrdsmanager'],
+    url='',
+    license='GPLv2',
+    author='Drew J. Sonne',
+    author_email='drew.sonne@nordcloud.com',
+    description=''
+)
